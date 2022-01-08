718d216eb4cef6ad645530952768df09a1c0f789
TODO <todo-actions[bot]@users.noreply.github.com>
2022-01-08 11:35:18 +0000

    Collect TODO comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// TODO: 이슈 생성테스트
+// TODO [$61d976f6a90fb80009d8c655]: 이슈 생성테스트
 //
 // This Issue is registered by Github Action
 // [https://github.com/marketplace/actions/todo-actions](https://github.com/marketplace/actions/todo-actions)
